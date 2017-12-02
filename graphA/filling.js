26ff95916c381f44869474e9675041825f61adcd
Dragan <iliya5376@mail.com>
2017-12-02 22:45:51 +0300

    feature(filling.js) add database filling using created procedures

--- graphA/filling.js
+++ graphA/filling.js
@@ -1,103 +1,111 @@
 var chalk = require('chalk');
 var async = require('async');
-var mock = require('mock-data');
-
-var rInt = mock.integer;
-var rStr = mock.string;
-var rDate = mock.date;
-
-var intGen = rInt(1, 100);
-var strGen = rStr(4, 10, 'Aa');
-var dateGen = rDate(2010, 2017, false, "YYYY-MM")
-
-//const retInt = intGen.generate(2);
-const retStr = strGen.generate(10000);
-//const retDateTime = dateGen.generate(2);
-
-//console.log(retInt, retStr, retDateTime);
-console.log('Generated...OK');
 
 const connection = require('./database')['dev'];
-const pgp = require('pg-promise')();
 
 const Client = require('pg').Client;
 const client = new Client(connection);
 
 var objectsTaksk = {
   clearTable: (done) => {
-    client.query('DELETE from \"nir-object\"', (err) => {
+    client.query('DELETE from \"nir-object\" CASCADE', (err) => {
       if (err) done(err);
-      console.log(chalk.green('Objects...Cleared...OK'));
+      console.log(chalk.green('OBJECTS TABLE...CLEARED...OK'));
       done(null);
     });
   },
   fillTable: (done) => {
 
-    // our set of columns, to be created only once, and then shared/reused,
-    // to let it cache up its formatting templates for high performance:
-    const cs = new pgp.helpers.ColumnSet(['name'], {
-      table: 'nir-object'
-    });
-
-    const values = retStr.map((item) => {
-      return {
-        name: item
-      };
-    });
-
-    // generating a multi-row insert query:
-    const query = pgp.helpers.insert(values, cs);
-    //=> INSERT INTO "tmp"("name") VALUES('..'),...,('...')
-
-    client.query(query, (err, res) => {
+    client.query('SELECT fill_objects(10000)', (err, res) => {
       if (err) done(err);
-      console.log(chalk.green('Objects...Filled...OK'));
+      console.log(chalk.green('OBJECTS TABLE...FILLED...OK'));
       done(null);
     });
   }
 };
 
 var linksTasks = {
-  clearTable: (done) => {
-    client.query('DELETE from \"nir-link\"', (err) => {
+  fillTable: (done) => {
+    client.query('SELECT fill_links(10000)', (err) => {
       if (err) done(err);
-      console.log(chalk.green('Links...Cleared...OK'));
+      console.log(chalk.green('LINKS TABLE...FILLED...OK'));
+      done(null);
+    });
+  }
+};
+
+
+var typesTasks = {
+  intTable: (done) => {
+    client.query('SELECT fill_int_table(50, 100)', (err) => {
+      if (err) done(err);
+      console.log(chalk.green('INT TABLE...FILLED...OK'));
       done(null);
     });
   },
-  fillTable: (done) => {
-    client.query('INSERT INTO \"nir-link\" (obj1, obj2, type) SELECT * FROM insert_links(10000)', (err) => {
+  stringTable: (done) => {
+    client.query('SELECT fill_string_table(7);', (err) => {
       if (err) done(err);
-      console.log(chalk.green('Objects...Filled...OK'));
+      console.log(chalk.green('STRING TABLE...FILLED...OK'));
+      done(null);
+    });
+  },
+  realTable: (done) => {
+    client.query('SELECT fill_real_table(50,100)', (err) => {
+      if (err) done(err);
+      console.log(chalk.green('REAL TABLE...FILLED...OK'));
+      done(null);
+    });
+  },
+  binarytable: (done) => {
+    client.query('SELECT fill_binary_table(6)', (err) => {
+      if (err) done(err);
+      console.log(chalk.green('BINARY TABLE...FILLED...OK'));
+      done(null);
+    });
+  },
+  datetimeTable: (done) => {
+    client.query('SELECT fill_datetime_table(\'2012-05-05 00:00:00\', \'2016-06-06 00:00:00\');', (err) => {
+      if (err) done(err);
+      console.log(chalk.green('DATETIME TABLE...FILLED...OK'));
       done(null);
     });
   }
 };
 
 client.connect((err) => {
-  if (err) console.log(chalk.red('Connection error...', err));
+  if (err) console.log(chalk.red('CONNECTION ERROR...', err));
 
   if (!err) {
-
+    console.log(chalk.yellow('CONNECTED...OK'));
     async.series([
       (done) => {
+        // Заполнение объектов
         async.series(objectsTaksk, (err, results) => {
           if (err) done(err);
           done(null);
-          console.log(chalk.green('Objects...Done...OK'));
         });
       },
       (done) => {
+        // Заполнение связей
         async.series(linksTasks, (err, results) => {
           if (err) done(err);
           done(null);
-          console.log(chalk.green('Links...Done...OK'));
         });
       }
     ], (err, results) => {
       if (err) console.log(chalk.red(err));
-      client.end();
-      if(!err) console.log(chalk.green('Done...OK'));
+
+      if (!err) {
+
+        // Заполнение таблиц со всеми типами(парпллельно)
+        async.parallel(typesTasks, (err, results) => {
+          if (err) console.log(chalk.red(err));
+
+          client.end();
+          if (!err) console.log(chalk.green('DATABASE ARE FILLED...Done...OK'));
+        });
+      }
     });
 
   }
