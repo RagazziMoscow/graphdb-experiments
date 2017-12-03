e70b76ea53e3d7be26890005a598cc4c8c37ad8f
Dragan <iliya5376@mail.com>
2017-12-03 15:11:05 +0300

    add select/join procedures

---
+++ graphA/sql/select_all_links.sql
@@ -0,0 +1,34 @@
+﻿CREATE OR REPLACE FUNCTION select_all_links() RETURNS table (obj text, attr text, type text, attr_value text) AS
+$BODY$
+DECLARE
+  link_record RECORD;
+BEGIN
+  FOR link_record IN SELECT link.link_id, object1.name AS obj1_name, object2.name AS obj2_name, link.type FROM "nir-link" link
+    JOIN "nir-object" object1 ON link.obj1 = object1.id
+    JOIN "nir-object" object2 ON link.obj2 = object2.id
+
+    LOOP
+      obj := link_record.obj1_name;
+      attr := link_record.obj2_name;
+      type := link_record.type;
+
+      IF type = 'int' THEN
+        SELECT value::text INTO attr_value  FROM "int" int_table WHERE int_table.link_id = link_record.link_id LIMIT 1;
+      END IF;
+      IF type = 'string' THEN
+        SELECT value INTO attr_value FROM "string" string_table WHERE string_table.link_id = link_record.link_id LIMIT 1;
+      END IF;
+      IF type = 'double' THEN
+        SELECT value::text INTO attr_value FROM "real" real_table WHERE real_table.link_id = link_record.link_id LIMIT 1;
+      END IF;
+      IF type = 'binary' THEN
+        SELECT value::text INTO attr_value FROM "binary" binary_table WHERE binary_table.link_id = link_record.link_id LIMIT 1;
+      END IF;
+      IF type = 'datetime' THEN
+        SELECT value INTO attr_value FROM "date-time" datetime_table WHERE datetime_table.link_id = link_record.link_id LIMIT 1;
+      END IF;
+      RETURN NEXT;
+  END LOOP;
+END;
+$BODY$
+LANGUAGE plpgsql;
---
+++ graphB/sql/select_all_links.sql
@@ -0,0 +1,40 @@
+﻿CREATE OR REPLACE FUNCTION select_all_links() RETURNS table (obj text, attr text, type text, attr_value text) AS
+$BODY$
+DECLARE
+  link_record RECORD;
+BEGIN
+  FOR link_record IN SELECT link.link_id, object_table.name AS obj_name,
+			    object_table.name AS attr_name,
+			    link.int, link.string, link.real,
+			    link.binary, link.datetime
+		     FROM "nir-link" link
+		     JOIN "nir-object" object_table ON link.obj1 = object_table.id
+		     LOOP
+      obj := link_record.obj_name;
+      attr := link_record.attr_name;
+
+      IF link_record.int IS NOT NULL THEN
+        attr_value := link_record.int::text;
+        type := 'int';
+      END IF;
+      IF link_record.string IS NOT NULL THEN
+        type := 'string';
+        attr_value := link_record.string;
+      END IF;
+      IF link_record.real IS NOT NULL THEN
+        type := 'real';
+        attr_value := link_record.real::text;
+      END IF;
+      IF link_record.binary IS NOT NULL THEN
+        type := 'binary';
+        attr_value := link_record.binary::text;
+      END IF;
+      IF link_record.datetime IS NOT NULL THEN
+        type := 'datetime';
+        attr_value := link_record.datetime;
+      END IF;
+      RETURN NEXT;
+  END LOOP;
+END;
+$BODY$
+LANGUAGE plpgsql;
